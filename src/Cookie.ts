bdb6c1a2bdf2e7f8469367d42306b7a1445e023e
Paradoxis <luke@paradoxis.nl>
2016-04-13 16:31:42 +0200

    Fresh start on development branch, no more JISON, 0.3.0

--- src/Cookie.ts
+++
@@ -1,9 +0,0 @@
-export interface ICookieOption {
-    domain?: string;
-    path?: string;
-    secure?: boolean;
-    httpOnly?: boolean;
-    expires?: Date;
-    maxAge?: number;
-    signed?: boolean;
-}
