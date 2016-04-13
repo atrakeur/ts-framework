8aa21b812c748e9e26dbaa9a9d1a7f9681756d5d
Paradoxis <luke@paradoxis.nl>
2016-04-13 17:31:06 +0200

    Added main file, updated package.json

--- src/ts-framework/Application.ts
+++ src/ts-framework/Application.ts
@@ -1,5 +1,7 @@
 /// <reference path="../../typings/main.d.ts" />
 
+import {Configuration} from "./Configuration";
+
 /**
  * TS-Framework application
  * This class is a wrapper around the default express server
@@ -7,6 +9,18 @@
  */
 export class Application
 {
+    /**
+     * Configuration object
+     * @type {Configuration}
+     */
+    private config: Configuration;
+
+    /**
+     * Express server we're creating a wrapper for
+     * @type {Express.Application}
+     */
+    private express: Express.Application;
+
     /**
      * Application constructor
      * Developer must define the default root directory of the application
