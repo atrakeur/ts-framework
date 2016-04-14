fbb60672d4d9d4f717b14bf6a55707979d8dbcba
Petr Tomášek <petrtomasek@Petr-MacBook-Pro.local>
2016-04-14 19:10:13 +0200

    Update HTTP, Request, Response. Adding ResultController file

--- src/ts-framework/ActionResult.ts
+++ src/ts-framework/ActionResult.ts
@@ -1,4 +1,89 @@
+import { Response } from "./Http";
+
+/**
+ * TS-Framework ActionResult
+ * This class contains redirect result
+ * Methods are calling from ResultController, when user want return content from controller
+ */
+export class RedirectResult implements IActionResult
+{
+    constructor(public url: string, public status: number = 302) {}
+    execute(response: Response) {
+        response.express.redirect(this.status, this.url);
+    }
+}
+
+/**
+ * TS-Framework ContentResult
+ * This class contains redirect result
+ * Methods are calling from ResultController, when user want return content from controller
+ */
+export class ContentResult implements IActionResult {
+    constructor(public content: string, public contentType?: string) {}
+    execute(response: Response) {
+        if (!!this.contentType) response.express.type(this.contentType);
+        response.express.send(this.content);
+    }
+}
+
+/**
+ * TS-Framework JsonResult
+ * This class contains redirect result
+ * Methods are calling from ResultController, when user want return content from controller
+ */
+export class JsonResult implements IActionResult {
+    constructor(public data: {}) {}
+    execute(response: Response) {
+        response.express.json(this.data);
+    }
+}
+
+/**
+ * TS-Framework FileResult
+ * This class contains redirect result
+ * Methods are calling from ResultController, when user want return content from controller
+ */
+export class FileResult implements IActionResult {
+    constructor(public path: string) {}
+    execute(response: Response) {
+        //TODO - now we need t get root dir from confirugation  
+        var file = ""; //path.join(app.root, this.path);
+        response.express.sendfile(file);
+    }
+}
+
+/**
+ * TS-Framework DownloadResult
+ * This class contains redirect result
+ * Methods are calling from ResultController, when user want return content from controller
+ */
+export class DownloadResult implements IActionResult {
+    constructor(public path: string, public filename?: string) {}
+    execute(response: Response) {
+        //TODO - now we need t get root dir from confirugation  
+        var file = ""; //path.join(app.root, this.path); 
+        response.express.attachment(file);
+        response.express.sendfile(file, this.filename);
+    }
+}
+
+/**
+ * TS-Framework ViewResult
+ * This class contains redirect result
+ * Methods are calling from ResultController, when user want return content from controller
+ */
+export class ViewResult implements IActionResult {
+    constructor(public template: string, public options?: {}) {}
+    execute(response: Response) {
+        response.express.render(this.template, this.options);
+    }
+}
+
 export interface IActionResult 
 {
-    //...
+    /**      
+     * Send Response type
+     * @param {Response} response
+     */    
+    execute(response: Response);
 }
--- src/ts-framework/Http.ts
+++ src/ts-framework/Http.ts
@@ -30,33 +30,125 @@ export interface IMultipleResultCallback<T>
     (err: Error, models: T[]); 
 }
 
-
+/**
+ * TS-Framework Request
+ * Contains basic method for working with HTTP
+ */
 export class Request
 {
-    private request: Express.Request;
+    public constructor(public express: Express.Response) {}
 }
 
-
+/**
+ * TS-Framework Response
+ * Contains basic method for working with HTTP
+ */
 export class Response
 {
     /**
      * Constructor for Response
      * @param {Express.Response} response
      */
-    public constructor(private response: Express.Response) {}
+    public constructor(public express: Express.Response) {}
 
     /**
-     * Redirect a client to a given path/url
-     * @param {string} path
+     * Constructor for Response
+     * @param {number} code
+     * @return {Express.Response}
      */
-    public redirect(path: string)
-    {
-        this.response.redirect(path);
+    public setStatus(code: number): Response {
+        this.express.status(code);
+        return this;
     }
-}
 
-/** @todo What's actually the difference between Response and Reply? The name states they basically do the same thing */
-export class Reply
-{
-    //...
+    /**
+     * Get Response status     
+     * @return {number}
+     */
+    public getStatus(): number {
+        return this.express.statusCode;
+    }
+
+    /**
+     * Set Response header
+     * @param {string} filed
+     * @param {string} value
+     * @return {Express.Response}
+     */
+    public setHeader(field: string, value: string): Response {
+        this.express.header(field, value);
+        return this;
+    }
+
+    /**
+     * Get Response header
+     * @param {string} filed     
+     * @return {string}
+     */
+    public getHeader(field: string): string {
+        return this.express.get(field);
+    }
+
+    /**
+     * Set Cookie
+     * @param {string} name
+     * @param {string} value
+     * @param {ICookieOption?} options     
+     * @return {Response}
+     */
+    public setCookie(name: string, value: string, options?: ICookieOption): Response {
+        this.express.cookie(name, value, options);
+        return this;
+    }
+
+    /**
+     * Remove cookie
+     * @param {string} name
+     * @param {string} path     
+     * @return {Response}
+     */
+    public removeCookie(name: string, path: string = '/'): Response {
+        this.express.clearCookie(name, { path: path });
+        return this;
+    }
+    
+    /**
+     * Set content type of Response
+     * @param {string} value          
+     * @return {Response}
+     */
+    public setContentType(value: string): Response {
+        this.express.type(value);
+        return this;
+    }
+
+    /**
+     * Set Response Links
+     * @param {} value          
+     * @return {Response}
+     */
+    public setLinks(links: {}): Response {
+        this.express.links(links);
+        return this;
+    }
+
+    /**
+     * Set Response Local
+     * @param {string} name
+     * @param {any} value          
+     * @return {Response}
+     */
+    public setLocal(name: string, value: any): Response {
+        this.express.locals[name] = value;
+        return this;
+    }
+
+    /**
+     * Get Response Local     
+     * @param {any} name          
+     * @return {Response}
+     */
+    public getLocal(name: string): any {
+        return this.express.locals[name];
+    }
 }
