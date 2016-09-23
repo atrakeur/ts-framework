0f2223322b20a24aab50c942a15ac4ac6240dabe
atrakeur <atrakeur@gmail.com>
2016-09-23 08:29:21 +0200

    feature: Firest draft of a input validation component

--- src/Http/Request.ts
+++ src/Http/Request.ts
@@ -8,6 +8,14 @@ export class Request
 {
     public constructor(public express: Express.Request) {}
 
+    public post(name: string) {
+        if (name == undefined) {
+            return this.express.body;
+        }
+
+        return this.express.body[name];
+    }
+
     public params() {
         return this.express.params
     }
--- src/Index.ts
+++ src/Index.ts
@@ -2,6 +2,7 @@ export * from "./Core/Index";
 export * from "./Configuration/Index";
 export * from "./Http/Index";
 export * from "./Controller/Index";
+export * from "./Validator/Index";
 export * from "./View/Index";
 
 export {Container, Inject} from "huject";
